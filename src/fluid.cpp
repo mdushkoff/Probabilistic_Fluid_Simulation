1d70b6da41752e2e4a94d52e99127e25c4b3fc9d
Millie-8911 <128894092+Millie-8911@users.noreply.github.com>
2025-03-10 19:25:22 -0700

    Save changes for advection in fluid.cpp

--- src/fluid.cpp
+++ src/fluid.cpp
@@ -12,9 +12,44 @@ inline float jacobi(float xl, float xr, float xt, float xb, float alpha, float b
     return (xl+xr+xt+xb+alpha*b)/beta;
 }
 
-void advect(vp_field *vp){
-    // TODO: Perform advection
+inline int asIdx(int i, int j, int k, int height, int channels){
+    return (j * height * channels) + (i * channels) + k;
 }
+
+void advect(vp_field *vp, vp_field *vp_out, double dt){
+    // TODO: Perform advection
+    int width = vp->x, height = vp->y, depth = vp->z;
+    float *field = vp->data;
+    float *new_field = vp_out->data;
+
+    for (int j = 1; j < height - 1; j++) {
+        for (int i = 1; i < width - 1; i++) { // Iterate through the inner part in the grid
+            int x_idx = asIdx(i, j, 0, height, depth); // Cell index for x
+            int y_idx = asIdx(i, j, 1, height, depth); // Cell index for y
+
+            float x_prev = i - dt * field[x_idx] / width; // Compute the where the fluid came from at the previous time stamp // Divided by h so can convert velocity into grid units
+            // Offset index to get velocity in y direction
+            float y_prev = j - dt * field[y_idx] / height;
+
+            x_prev = std::max(0.5f, std::min<float>(width - 1.5, x_prev)); // Get the center of cooridinates and prevent out-of-bounds errors
+            y_prev = std::max(0.5f, std::min<float>(height - 1.5, y_prev));
+
+            int i0 = int(x_prev), j0 = int(y_prev);
+            int i1 = i0 + 1, j1 = j0 + 1; // Find the integer grid coordinates surrounding (x, y)
+            float sx = x_prev - i0, sy = y_prev - j0; // Represent the fractional distances from (i0, j0)
+
+            // Bilinear Interpolation
+            for (int vel_dir = 0; vel_dir < 2; vel_dir++) {
+                new_field[asIdx(i, j, vel_dir, height, depth)] = 
+                    (1 - sx) * (1 - sy) * field[asIdx(i0, j0, vel_dir, height, depth)] +
+                    sx * (1 - sy) * field[asIdx(i1, j0, vel_dir, height, depth)] +
+                    (1 - sx) * sy * field[asIdx(i0, j1, vel_dir, height, depth)] +
+                    sx * sy * field[asIdx(i1, j1, vel_dir, height, depth)];
+            }
+        }
+    }
+}
+
 
 void diffuse(vp_field *vp, float viscosity){
     // TODO: Perform diffusion
@@ -35,8 +70,8 @@ void subtractPressureGradient(vp_field *vp){
 void simulate_fluid_step(vp_field *vp, float dt, float viscosity){
     // Execute operators in order
     advect(vp);
-    diffuse(vp, viscosity);
+    // diffuse(vp, viscosity);
     //addForces(vp_field, forces);  // TODO: eventually add forces
-    computePressure(vp);
-    subtractPressureGradient(vp);
+    // computePressure(vp);
+    // subtractPressureGradient(vp);
 }
