dcbc04c8b40dd443751d21dea1779abb548219df
Millie-8911 <128894092+Millie-8911@users.noreply.github.com>
2025-03-15 00:44:39 -0700

    Added advection stride

--- src/fluid.cu
+++ src/fluid.cu
@@ -11,7 +11,6 @@
 
 // Definitions
 #define BLOCK_SIZE (256)
-#define GRID_SIZE (16)
 
 __device__ int asIdx(int i, int j, int k, int width, int channels) {
     return (j * width * channels) + (i * channels) + k;
@@ -26,90 +25,46 @@ __global__ void advect(vp_field *vp, vp_field *vp_out, double dt){
     float fwidth = (float)width;
     float fheight = (float)height;
 
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    int index_x = blockDim.x * blockIdx.x + threadIdx.x;
+    int index_y = blockDim.y * blockIdx.y + threadIdx.y;
 
-    if (i >= width || j >= height) return;
+    int stride_x = blockDim.x * gridDim.x;
+    int stride_y = blockDim.y * gridDim.y;
 
-    int x_idx = asIdx(i, j, 0, width, depth);
-    int y_idx = asIdx(i, j, 1, width, depth);
 
-    float x_prev = (float)i - dt * field[x_idx] / fwidth;
-    float y_prev = (float)j - dt * field[y_idx] / fheight;
+    if (index_x >= width || index_y >= height) return;
 
-    float xrem = fmodf(fmodf(x_prev, fwidth) + fwidth, fwidth);
-    float yrem = fmodf(fmodf(y_prev, fheight) + fheight, fheight);
-    x_prev = xrem;
-    y_prev = yrem;
+    for (int j = index_y; j < height; j += stride_y) {
+        for (int i = index_x; i < width; i += stride_x) { 
+            int x_idx = asIdx(i, j, 0, width, depth);
+            int y_idx = asIdx(i, j, 1, width, depth);
 
-    int i0 = int(x_prev), j0 = int(y_prev);
-    int i1 = (i0 + 1) % width, j1 = (j0 + 1) % height;
-    float sx = x_prev - (float)i0, sy = y_prev - (float)j0;
+            float x_prev = (float)i - dt * field[x_idx] / fwidth;
+            float y_prev = (float)j - dt * field[y_idx] / fheight;
 
-    for (int vel_dir = 0; vel_dir < 2; vel_dir++) {
-        new_field[asIdx(i, j, vel_dir, width, depth)] =
-            (1 - sx) * (1 - sy) * field[asIdx(i0, j0, vel_dir, width, depth)] +
-            sx * (1 - sy) * field[asIdx(i1, j0, vel_dir, width, depth)] +
-            (1 - sx) * sy * field[asIdx(i0, j1, vel_dir, width, depth)] +
-            sx * sy * field[asIdx(i1, j1, vel_dir, width, depth)];
+            float xrem = fmodf(fmodf(x_prev, fwidth) + fwidth, fwidth);
+            float yrem = fmodf(fmodf(y_prev, fheight) + fheight, fheight);
+            x_prev = xrem;
+            y_prev = yrem;
+
+            int i0 = int(x_prev), j0 = int(y_prev);
+            int i1 = (i0 + 1) % width, j1 = (j0 + 1) % height;
+            float sx = x_prev - (float)i0, sy = y_prev - (float)j0;
+
+            for (int vel_dir = 0; vel_dir < 2; vel_dir++) {
+                new_field[asIdx(i, j, vel_dir, width, depth)] =
+                    (1 - sx) * (1 - sy) * field[asIdx(i0, j0, vel_dir, width, depth)] +
+                    sx * (1 - sy) * field[asIdx(i1, j0, vel_dir, width, depth)] +
+                    (1 - sx) * sy * field[asIdx(i0, j1, vel_dir, width, depth)] +
+                    sx * sy * field[asIdx(i1, j1, vel_dir, width, depth)];
+            }
+        }
     }
 }
 
 __global__ void advect_color(vp_field *image, vp_field *out, vp_field *vp, float dt){
     // TODO: Perform color advection
     // Get dimensions
-    int iwidth = image->x, iheight = image->y, idepth = image->z;
-    int vwidth = vp->x, vheight = vp->y, vdepth = vp->z;
-
-    // Float constants
-    float fiwidth = (float)iwidth;
-    float fiheight = (float)iheight;
-    float fvwidth = (float)vwidth;
-    float fvheight = (float)vheight;
-    float viw = (fvwidth/fiwidth); // Fractional velocity width to image width
-    float vih = (fvheight/fiheight); // Fractional velocity height to image height
-
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
-
-    if (i >= iwidth || j >= iheight) return; // Out-of-bounds check
-
-    int vi = (int)((float)i * viw);
-    int vj = (int)((float)j * vih);
-
-    // Get velocity indices
-    int vx_idx = asIdx(vi, vj, 0, vwidth, vdepth); // Cell index for x
-    int vy_idx = asIdx(vi, vj, 1, vwidth, vdepth); // Cell index for y
-
-    // Calculate previous coordinate
-    float x_prev = (float)i - (dt/viw) * vp->data[vx_idx] / fiwidth; // Compute the where the color came from at the previous time stamp // Divided by h so can convert velocity into grid units
-    float y_prev = (float)j - (dt/vih) * vp->data[vy_idx] / fiheight;
-
-    // Clamp mode
-    //x_prev = std::max(0.5f, std::min<float>(fiwidth - 1.5, x_prev)); // Get the center of cooridinates and prevent out-of-bounds errors
-    //y_prev = std::max(0.5f, std::min<float>(fiheight - 1.5, y_prev));
-
-    // Wrapping mode
-    x_prev = fmod(fmod(x_prev, fiwidth)+fiwidth, fiwidth);
-    y_prev = fmod(fmod(y_prev, fiheight)+fiheight, fiheight);
-    //float xrem = fmod(x_prev, fiwidth);
-    //float yrem = fmod(y_prev, fiheight);
-    //x_prev = (x_prev >= 0) ? xrem : xrem + fiwidth;
-    //y_prev = (y_prev >= 0) ? yrem : yrem + fiheight;
-
-    int i0 = int(x_prev), j0 = int(y_prev);
-    int i1 = (i0 + 1) % iwidth, j1 = (j0 + 1) % iheight; // Find the integer grid coordinates surrounding (x, y)
-    float sx = x_prev - (float)i0, sy = y_prev - (float)j0; // Represent the fractional distances from (i0, j0)
-
-    // Apply velocity to each channel
-    for (int k=0; k<idepth; k++){
-        // Bilinear interpolation
-        itmp->data[asIdx(i, j, k, iwidth, idepth)] = 
-            (1 - sx) * (1 - sy) * image->data[asIdx(i0, j0, k, iwidth, idepth)] +
-            sx * (1 - sy) * image->data[asIdx(i1, j0, k, iwidth, idepth)] +
-            (1 - sx) * sy * image->data[asIdx(i0, j1, k, iwidth, idepth)] +
-            sx * sy * image->data[asIdx(i1, j1, k, iwidth, idepth)];
-    }
 }
 
 __global__ void diffuse(vp_field *vp, vp_field *vp_out, float viscosity, float dt){
